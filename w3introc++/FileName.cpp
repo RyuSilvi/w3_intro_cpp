d8b9aa47419231d410d357946d6ca4afce82cca4
Ryuu <Aleksander.hagland@hotmail.com>
2025-08-17 23:52:06 +0200

    does this work now?

--- w3introc++/FileName.cpp
+++ w3introc++/FileName.cpp
@@ -2,16 +2,6 @@
 using namespace std;
 
 int main() {
-	int myNum = 12;
-	double myDoubleNum = 12.99;
-	char myLetter = 'D';
-	string myText = "Hello World! \nI am learning c++!";
-	bool myBoolean = true;
-	cout << "int " << myNum << "\n";
-	cout << "double " << myDoubleNum << "\n";
-	cout << "char " << myLetter << "\n";
-	cout <<  myText << "\n";
-	cout << "bool " << myBoolean << "\n";
-
+	
 	return 0;
 }
